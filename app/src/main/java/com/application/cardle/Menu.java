0c70be35e43467f8091db3a21a5b28ab13648253
Dursam <48018775+Dursam@users.noreply.github.com>
2022-03-18 23:00:35 +0100

    Button Panel in Menu

--- app/src/main/java/com/application/cardle/Menu.java
+++ app/src/main/java/com/application/cardle/Menu.java
@@ -8,36 +8,32 @@ import android.os.Bundle;
 
 public class Menu extends AppCompatActivity {
 
-    private RecyclerView DeckRV;
-
-    // Arraylist for storing data
-    private ArrayList<DeckModel> DeckModelArrayList;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
-        DeckRV = findViewById(R.id.Deck);
+        RecyclerView deckRV = findViewById(R.id.Deck);
 
         // here we have created new array list and added data to it.
-        DeckModelArrayList = new ArrayList<>();
-        DeckModelArrayList.add(new DeckModel("DSA in Java"));
-        DeckModelArrayList.add(new DeckModel("Java Deck"));
-        DeckModelArrayList.add(new DeckModel("C++ Deck"));
-        DeckModelArrayList.add(new DeckModel("DSA in C++"));
-        DeckModelArrayList.add(new DeckModel("Kotlin for Android"));
-        DeckModelArrayList.add(new DeckModel("Java for Android"));
-        DeckModelArrayList.add(new DeckModel("HTML and CSS"));
+        // Arraylist for storing data
+        ArrayList<DeckModel> deckModelArrayList = new ArrayList<>();
+        deckModelArrayList.add(new DeckModel("DSA in Java"));
+        deckModelArrayList.add(new DeckModel("Java Deck"));
+        deckModelArrayList.add(new DeckModel("C++ Deck"));
+        deckModelArrayList.add(new DeckModel("DSA in C++"));
+        deckModelArrayList.add(new DeckModel("Kotlin for Android"));
+        deckModelArrayList.add(new DeckModel("Java for Android"));
+        deckModelArrayList.add(new DeckModel("HTML and CSS"));
 
         // we are initializing our adapter class and passing our arraylist to it.
-        DeckAdapter DeckAdapter = new DeckAdapter(this, DeckModelArrayList);
+        DeckAdapter DeckAdapter = new DeckAdapter(this, deckModelArrayList);
 
         // below line is for setting a layout manager for our recycler view.
         // here we are creating vertical list so we will provide orientation as vertical
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
 
         // in below two lines we are setting layoutmanager and adapter to our recycler view.
-        DeckRV.setLayoutManager(linearLayoutManager);
-        DeckRV.setAdapter(DeckAdapter);
+        deckRV.setLayoutManager(linearLayoutManager);
+        deckRV.setAdapter(DeckAdapter);
     }
 }
